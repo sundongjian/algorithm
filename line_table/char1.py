d5324a20072624fb20f072e80753e2b65eb6de3a
sdj <xiaobomentu@163.com>
2018-05-04 19:17:07 +0800

    add all

--- line_table/char1.py
+++ line_table/char1.py
@@ -105,6 +105,107 @@ class LList:
                 yield
             p=p.next
 
+    #链表之间的操作
+    def rev(self):
+        p=None
+        while self._head is not None:
+            q=self._head
+            self._head=q.next
+            q.next=p#书上错误
+            p=q
+        self._head=p
+
+    #排序
+    def sort(self):#参考char2的排序
+        if self._head is None:
+            return
+        crt=self._head.next
+        while crt is not None:
+            x=crt.elem
+            p=self._head
+            while p is not crt and p.elem <=x:#跳过小元素
+                p=p.next#找到第一个大于等于x的所在位置p
+            while p is not crt:#将[p,crt-1]的元素后移
+                y=p.elem
+                p.elem=x
+                x=y
+                p=p.next
+            crt.elem=x#填上最后一个crt位置
+            crt=crt.next
+
+    #下面两个排序为粘贴，有点难
+    def sort1(self):
+        p = self._head
+        if p is None or p.next is None:
+            return
+        rem = p.next
+        p.next = None
+        while rem is not None:
+            p = self._head
+            q = None
+            while rem is not None and p.elem <= rem.elem:
+                q = p
+                p = p.next
+            if q is None:
+                self._head = rem
+            else:
+                q.next = rem
+            q = rem
+            rem = rem.next
+            q.next = p
+
+    # 第三种排序算法
+    def sort2(self):
+        list1 = copy.deepcopy(self)
+        if list1._head.next is None:
+            return
+        list1._head.next.next = None
+
+        if list1._head.next.elem < list1._head.elem:
+            a = list1._head
+            list1._head = list1._head.next
+            list1._head.next = a
+            list1._head.next.next = None
+
+        temp = self._head.next.next
+
+        while temp is not None:
+            p = list1._head
+            q = list1._head.next
+            if temp.elem < list1._head.elem:
+                a = temp.next
+                temp.next = list1._head
+                list1._head = temp
+                temp = a
+                if temp is not None:
+                    print(temp.elem)
+                    list1.printall()
+            elif temp.elem >= list1._head.elem:
+                while q is not None:
+                    if q.elem >= temp.elem:
+                        a = temp.next
+                        temp.next = q
+                        p.next = temp
+                        temp = a
+                        break
+                    elif q.elem < temp.elem:
+                        q = q.next
+                        p = p.next
+                if q is None:
+                    p.next = temp
+                    a = temp.next
+                    temp.next = None
+                    temp = a
+        self._head = list1._head
+
+
+
+
+
+
+
+
+
 
 #增加尾节点
 class LRist(LList):
@@ -192,13 +293,6 @@ class LCList():
             if p is self.rear:
                 break
             p=p.next
-
-
-#双链表
-class DLNode(LNode):
-    def __init__(self,elem,pre=None,next_=None):
-        LNode.__init__(self,elem,next_)
-        self.prev=prev
 
 
 
